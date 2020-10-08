4fc53f5ba86cb2379a2d8d177b092fd41bf7b9b9
Brock Byrd <brockbyrdd@gmail.com>
2020-10-08 17:05:09 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,4 +8,41 @@ class Dog
     end
   end
 
+  def self.create_table
+
+  end
+
+  def self.drop_table
+
+  end
+
+
+  def save
+
+  end
+
+  def self.create
+
+  end
+
+  def self.new_from_db
+
+  end
+
+  def self.find_by_id
+
+  end
+
+  def self.find_or_create_by
+
+  end
+
+  def self.find_by_name
+
+  end
+
+  def update
+
+  end
+
 end
